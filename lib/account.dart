20b5a258c921e0167755b9a6b4614c0218c054f3
Kelvin Dwipangga <144301420+klvnd@users.noreply.github.com>
2023-11-28 22:19:30 +0700

    update

--- lib/account.dart
+++ lib/account.dart
@@ -1,10 +1,13 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-class Account extends StatelessWidget { 
-  const Account ({ Key? key }) : super(key: key); 
+class Account extends StatelessWidget {
+  const Account({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         leading: const BackButton(color: Colors.black),
@@ -17,28 +20,45 @@ class Account extends StatelessWidget {
             fontFamily: 'ComicSansMS',
             fontWeight: FontWeight.bold,
           ),
-          ),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15.0),
-          )
         ),
+        elevation: 0,
+      ),
       body: Container(
         height: double.infinity,
         width: double.infinity,
         decoration: const BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('images/background1.png'), 
-            fit: BoxFit.cover
+            image: AssetImage('images/background1.png'),
+            fit: BoxFit.cover,
           ),
         ),
-        child: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-            ],
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 0),
+              child: Container(
+                width: screenWidth,
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: const Icon(Icons.star, size: 50),
+              ),
+            ),
+            // Your other content here
+          ],
         ),
       ),
     );
   }
-} 
+}
